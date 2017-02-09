e67945fcce29c4c8f6752180d5cb855be1a0b5a9
admin <admin@DESKTOP-NNLVJHQ>
2017-02-09 12:20:07 -0500

    Add project files.

---
+++ SignalrPractice/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SignalrPractice.Startup))]
+namespace SignalrPractice
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
